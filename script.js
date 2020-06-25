8ca16241e5d87132a004e78b990b5079d935ea4c
Victoria Kee <victoriakee95@gmail.com>
2020-06-25 11:17:44 -0500

    Fix page

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-let lightbulb = document.getElementById('lightbulb');
-
 function bulbSwitch() {
-  if (lightbulb.src.match("img/lighton.jpg")) {
+  const lightbulb = document.getElementById('lightbulb');
+  if(lightbulb.src = "img/lighton.jpg") {
     lightbulb.src = "img/lightoff.jpg";
   } else {
     lightbulb.src = "img/lighton.jpg";
