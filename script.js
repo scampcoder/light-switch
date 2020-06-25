e51e725f0984e6cdf3991e9c735d986f5df88e26
Victoria Kee <victoriakee95@gmail.com>
2020-06-25 09:51:18 -0500

    Write main function for switch

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 let lightbulb = document.getElementById('lightbulb');
+
+function bulbSwitch() {
+  if (lightbulb.src.match("lighton.jpeg")) {
+    lightbulb.src = "lightoff.jpeg";
+  } else {
+    lightbulb.src = "lighton.jpeg";
+  }
+}
